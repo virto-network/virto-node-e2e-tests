409df453bf3848530fdb7cab1ad7a61f36bb0d80
Pablo Andrés Dorado Suárez <hola@pablodorado.com>
2024-03-12 20:41:45 -0500

    change(lib:txSend): enable logging for sending extrinsics method

--- lib/tx-send.ts
+++ lib/tx-send.ts
@@ -3,22 +3,31 @@ import { KeyringPair } from "@polkadot/keyring/types";
 
 export function signTxSendAndWait(
   tx: SubmittableExtrinsic<"promise">,
-  signer: KeyringPair
+  signer: KeyringPair,
+  useLogs = false
 ) {
-  // console.log(`[${signer.address}] is submitting`, tx.args?.at(0)?.toHuman());
+  if (useLogs) {
+    console.log(`[${signer.address}] is submitting`, tx.args?.at(0)?.toHuman());
+  }
 
   return new Promise<void>(async (resolve) => {
     const unsub = await tx.signAndSend(signer, (result) => {
-      // console.log(`Current status is ${result.status}`);
+      if (useLogs) {
+        console.log(`Current status is ${result.status}`);
+      }
 
       if (result.status.isInBlock) {
-        // console.log(
-        //   `Transaction included at blockHash ${result.status.asInBlock}`
-        // );
+        if (useLogs) {
+          console.log(
+            `Transaction included at blockHash ${result.status.asInBlock}`
+          );
+        }
       } else if (result.status.isFinalized) {
-        // console.log(
-        //   `Transaction finalized at blockHash ${result.status.asFinalized}`
-        // );
+        if (useLogs) {
+          console.log(
+            `Transaction finalized at blockHash ${result.status.asFinalized}`
+          );
+        }
 
         unsub();
         resolve();
@@ -26,3 +35,8 @@ export function signTxSendAndWait(
     });
   });
 }
+
+signTxSendAndWait.withLogs = (
+  tx: SubmittableExtrinsic<"promise">,
+  signer: KeyringPair
+) => signTxSendAndWait(tx, signer, true);
